579ed4a3f89bcb8fe08b17dc0f22f8bdcff6124e
LiemSgod <liemro9x@gmail.com>
2024-04-24 08:40:40 +0700

    fix admin-page

--- js/initialization.js
+++ js/initialization.js
@@ -632,27 +632,17 @@ function createAdminAccount() {
   if (!accounts) {
     accounts = [];
     accounts.push({
-      fullname: "Hoàng Gia Bảo",
-      phone: "hgbaodev",
+      fullname: "Nguyễn Thanh Liêm",
+      phone: "accountadm",
       password: "123456",
-      address: "https://github.com/hgbaodev",
-      email: "musicanime2501@gmail.com",
+      address: "Quận 9, TP Hồ Chí Minh",
+      email: "liemro9x@gmail.com",
       status: 1,
       join: new Date(),
       cart: [],
       userType: 1,
     });
-    accounts.push({
-      fullname: "Trần Nhật Sinh",
-      phone: "0123456789",
-      password: "123456",
-      address: "",
-      email: "",
-      status: 1,
-      join: new Date(),
-      cart: [],
-      userType: 1,
-    });
+
     localStorage.setItem("accounts", JSON.stringify(accounts));
   }
 }
